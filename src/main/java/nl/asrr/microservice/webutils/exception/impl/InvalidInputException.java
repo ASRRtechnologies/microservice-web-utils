e681a056fbd36d47568eb1e3e0a856ae5cffd588
Satyam <satyam.pramod.mohan@gmail.com>
2019-02-03 00:15:54 +0100

    Add constructors for InvalidInputException

--- src/main/java/nl/asrr/microservice/webutils/exception/impl/InvalidInputException.java
+++ src/main/java/nl/asrr/microservice/webutils/exception/impl/InvalidInputException.java
@@ -7,8 +7,18 @@ public class InvalidInputException extends RuntimeException {
 
     private final String property;
 
-    private final String errorCode;
+    private final String errorCode = "Invalid";
 
     private final String errorMessage;
+
+    public InvalidInputException(String property) {
+        this.property = property;
+        this.errorMessage = String.format("input for %s is invalid", property);
+    }
+
+    public InvalidInputException(String property, String errorMessage) {
+        this.property = property;
+        this.errorMessage = errorMessage;
+    }
 
 }
