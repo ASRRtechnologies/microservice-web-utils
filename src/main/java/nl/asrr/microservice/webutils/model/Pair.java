0781a3cc8901d17f306e1bc9e08a819e4d963f60
Satyam <satyam.pramod.mohan@gmail.com>
2019-04-06 04:40:12 +0200

    Remove interface

--- src/main/java/nl/asrr/microservice/webutils/model/Pair.java
+++ src/main/java/nl/asrr/microservice/webutils/model/Pair.java
@@ -4,19 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Map;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Pair<K, V> implements Map.Entry<K, V> {
+public class Pair<K, V> {
 
     private K key;
 
     private V value;
-
-    public V setValue(V value) {
-        return this.value = value;
-    }
 
 }
